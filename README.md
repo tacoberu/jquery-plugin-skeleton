7fb0706243e3b26dd1b214925d407b3b7ee2ee81
Martin Takáč <martin@takac.name>
2015-01-03 18:05:37 +0100

    add: sample pattern

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ jquery-plugin-skeleton
 ======================
 
 Skeleton for jquery plugin with potentiality overiding public method.
+
+File xpattern.jquery.js is code pattern for creating new plugins, with only necessary code.
+
+evidentationNumber.jquery.js is sample implementation and evidentationNumberEx.jquery.js is sample extend of it.
---
+++ js/pattern.jquery.js
@@ -0,0 +1,115 @@
+/**
+ *	Vzor pro vytvoření jQuery pluginu stavějící na prototypech.
+ *
+ *  Konstrukční funkce je funkce vytvářející instanci třídy objektu.
+ *	Instanční funkce je funkce vytváčející instanci objektu. Obvykle ji pojmenováváme
+ *		init, a volá se podle zvyklostí, v případě, kdy jako první parametr je
+ *		tabulka nastavení, případně je prázdný.
+ *	Option funkce je funkce, která se volá jménem uvedeným jako první parametr.
+ *		V případě, že druhý parametr je prázdný, funguje jako getter, v případě,
+ *		že je vyplněn funguje jako setter.
+ *
+ *  Licensed under both the MIT license and the GNU GPLv2 (same as jQuery: http://jquery.org/license)
+ *
+ *	@author Martin Takáč
+ */
+if (jQuery)(function($) {
+
+
+
+	/**
+	 * Plugin pro dekoraci input prvku na vkládání rodného čísla.
+	 */
+	$.fn.xpattern = (function ()
+	{
+
+
+		/**
+		 * Inicializate function for instantion.
+		 *
+		 * @access Private
+		 * @this DOMElement
+		 * @param options Instance pluginu pro ten který konkrétní element.
+		 *
+		 * @return DOMElement
+		 */
+		function init(options)
+		{
+			// own code
+
+			return options;
+		};
+
+
+
+		/**
+		 * Construct Function
+		 *
+		 * Touto funkcí ovládáme instanci, vytváření, čtení a nastavování
+		 * atributů a hodnot. Pracujeme vždy s kolekcí elementů, což znamená,
+		 * že zásahy se týkají vždy celé kolekce.
+		 *
+		 * @this Colection of Elements
+		 * @param string | object method Název akce, nebo nastavení konstruktoru.
+		 */
+		function xpattern(method)
+		{
+			//	Instantion method - create instance.
+			if (typeof method === 'object' || !method) {
+
+				/**
+				 * Defaultní konfigurace pluginu.
+				 * Lokální proměnná.
+				 */
+				var defaults = {
+					sample: xpattern.prototype.sample,
+					separator: '/',
+					version: '0.1'
+				};
+
+				/**
+				 * Zpracování všech elementů selektoru. Pro každý element vytvoříme
+				 * instanci pomocí funkce `init'. Výslednou instanci nalepíme na objekt elementu.
+				 * $.fn.extend({}, ... ) je nutné, aby se vytvořila vždy unikátní instance objektu.
+				 *
+				 * @param this DOMElement
+				 * @param index Of colection of Elements
+				 * @param el DOMElement
+				 */
+				return this.each(function(index, el) {
+					//	Zapsat instanci do objektu.
+					this.xpattern = init.call(this, $.fn.extend({}, defaults, method || {}));
+					return this;
+				});
+			}
+			//	Option method
+			else if (xpattern.prototype[method]) {
+				var result = [];
+				this.each(function(index, el) {
+					result.push(xpattern.prototype[method].apply(el.xpattern, Array.prototype.slice.call(arguments, 1)));
+				});
+				return result;
+			}
+			else {
+				$.error('Method ' + method + ' does not exist on jQuery.tacoCombobox');
+			}
+		}
+
+
+
+		/**
+		 * @this instance pluginu.
+		 * @param DOMElement element
+		 * @return DOMElement element
+		 */
+		xpattern.prototype.sample = function(element)
+		{
+			return element;
+		}
+
+
+		return xpattern;
+	})();
+
+
+})(jQuery);
